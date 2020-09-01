6464f700a5f2ea0fc0ac7b6246f30d65b25c7686
Samuel Capusesera <samueldelimapessoal@gmail.com>
2020-09-01 17:28:39 -0300

    Exception Handling - delete

--- src/main/java/com/samuellima/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/samuellima/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.samuellima.course.resources.exceptions;
 
+import com.samuellima.course.services.exceptions.DatabaseException;
 import com.samuellima.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,5 +19,13 @@ public class ResourceExceptionHandler {
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
             return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
     }
 }
--- src/main/java/com/samuellima/course/services/UserService.java
+++ src/main/java/com/samuellima/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.samuellima.course.services;
 
 import com.samuellima.course.entities.User;
 import com.samuellima.course.repositories.UserRepository;
+import com.samuellima.course.services.exceptions.DatabaseException;
 import com.samuellima.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,10 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) { throw new ResourceNotFoundException(id); }
+        catch (DataIntegrityViolationException e) {throw new DatabaseException(e.getMessage()); }
     }
 
     public User update(Long id, User obj) {
